2d9b847f8381c892344c986d99023201d9966e90
aaaa1024 <suga.ryota.24@shizuoka.ac.jp>
2025-12-11 23:12:09 +0900

    send infrared datas

--- send_data/send_data.py
+++ send_data/send_data.py
@@ -6,11 +6,13 @@ import time
 # from ..test.sensor import get_sensor_data
 from sensor.sensor import get_sensor_data
 from data.get_data import get_people_count
+from send_infrared.send_infrared import Send_infrared
 
 CHANNEL_ID = 96959  # チャンネルID
 WRITE_KEY = "60a58a651c5cdb19"  # ライトキー
-
 am = Ambient(CHANNEL_ID, WRITE_KEY)
+
+send_infrared = Send_infrared()
 
 print("データ送信を開始します (Ctrl+C で停止)")
 
@@ -20,16 +22,17 @@ try:
         temp, humid = get_sensor_data()
         people = get_people_count()
         print(people)
+        # cpntrol airconditonor and get flag
+        airconditonor_flag = send_infrared.control_airconditioner(people)
 
         # データ送信 (am.send(...) の部分は変更なし)
-        r = am.send({"d1": temp, "d2": humid, "d3": people})
+        r = am.send({"d1": temp, "d2": humid, "d3": people, "d4": airconditonor_flag})
 
         if r.status_code == 200:
             print(f"送信成功: 温度={temp:.2f}, 湿度={humid:.2f}")
         else:
             print(f"送信失敗: {r.status_code}")
-
-        time.sleep(5)
+        time.sleep(10)
 
 except KeyboardInterrupt:
     print("\n停止しました")
