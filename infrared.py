821fc9f34eb96c14787cfb4bde581ee3be9142da
aaaa1024 <suga.ryota.24@shizuoka.ac.jp>
2025-12-24 15:42:28 +0900

    READMEにcodes.json関係を記載

--- infrared.py
+++ infrared.py
@@ -1,9 +1,32 @@
 import subprocess
-import json
+import time
+from data.get_data import get_people_count
 
-open_json = open("./codes.json", "r")
-keys = json.load(open_json)
+# by people count backet
+keys = [
+    "off",
+    "temperture=24.0",
+    "temperture=23.5",
+    "temperture=23.0",
+    "temperture=22.5",
+    "temperture=22.0",
+    "temperture=21.5",
+    "temperture=21.0",
+    "temperture=20.5",
+    "temperture=20.0",
+]
 
-print(keys["off"])
-# command
-subprocess.run(["cgir", "send", "off"])
+pres_people = -1
+
+while True:
+    current_people = get_people_count()
+    if current_people >= 0:
+        if pres_people != current_people and pres_people == 0:
+            subprocess.run(["cgir", "send", "heat-om"])
+            time.sleep(1)
+        if pres_people != current_people:
+            # command
+            print(current_people)
+            subprocess.run(["cgir", "send", keys[current_people]])
+            pres_people = current_people
+    time.sleep(10)
